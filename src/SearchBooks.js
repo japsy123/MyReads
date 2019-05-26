d849cda634a4dd2777e9f79b696dba5a6516b14b
Jal Panchal <jal08panchal@gmail.com>
2019-05-26 06:42:05 -0700

    Setting state for SearchBook

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import { search } from "./BooksAPI";
 export default class SearchBooks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookId: ""
+    };
+  }
   render() {
     return (
       <div className="search-books">
@@ -11,15 +17,11 @@ export default class SearchBooks extends Component {
           </Link>
 
           <div className="search-books-input-wrapper">
-            {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.bookId}
+            />
           </div>
         </div>
         <div className="search-books-results">
